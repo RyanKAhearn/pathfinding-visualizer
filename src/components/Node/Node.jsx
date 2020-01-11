6c81a17859ba30a526f70f6a569316ff891138cf
ryan_ <ahearn@hartford.edu>
2020-01-11 02:38:27 -0500

    added animation to creating walls, altered some styles and changed indentation to 2 spaces.

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import './Node.css';
 
 export const Node = React.forwardRef((props, ref) => {
-    const {
-        row,
-        col,
-        isFinish,
-        isStart,
-        isWall,
-    } = props;
-    const extraClassName = isFinish ? 'node-finish' : isStart ? 'node-start' : isWall ? 'node-wall' : '';
-    return (
-        <div
-            id={`node-${row}-${col}`}
-            className={`node ${extraClassName}`}
-            ref={ref}
-            draggable="false"></div>
-    );
+  const {
+    row,
+    col,
+    isFinish,
+    isStart,
+    isWall,
+  } = props;
+  const extraClassName = isFinish ? 'node-finish' : isStart ? 'node-start' : isWall ? 'node-wall' : '';
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      ref={ref}
+      draggable="false"></div>
+  );
 })
--- src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -11,128 +11,128 @@ const FINISH_NODE_COL = 35;
 
 
 export default function PathfindingVisualizer(props) {
-    const [state, setState] = useState({
-        grid: getInitialGrid(),
+  const [state, setState] = useState({
+    grid: getInitialGrid(),
+    mouseIsPressed: false
+  });
+
+  useEffect(() => {
+    const handleMouseDown = (e) => {
+      e.preventDefault();
+      if (e.target.id.startsWith('node')) {
+        let parts = e.target.id.split('-');
+        setState(s => ({
+          ...s,
+          grid: getNewGridWithWallToggled(s.grid, parseInt(parts[1]), parseInt(parts[2])),
+          mouseIsPressed: true
+        }));
+      } else {
+        setState(s => ({
+          ...s,
+          mouseIsPressed: true
+        }));
+      }
+    }
+    document.addEventListener('mousedown', handleMouseDown);
+
+    const handleMouseUp = () => {
+      setState(s => ({
+        ...s,
         mouseIsPressed: false
-    });
+      }));
+    }
+    document.addEventListener('mouseup', handleMouseUp);
 
-    useEffect(() => {
-        const handleMouseDown = (e) => {
-            e.preventDefault();
-            if (e.target.id.startsWith('node')) {
-                let parts = e.target.id.split('-');
-                setState(s => ({
-                    ...s,
-                    grid: getNewGridWithWallToggled(s.grid, parseInt(parts[1]), parseInt(parts[2])),
-                    mouseIsPressed: true
-                }));
-            } else {
-                setState(s => ({
-                    ...s,
-                    mouseIsPressed: true
-                }));
-            }
+
+    const handleMouseEnter = (e) => {
+      setState(s => {
+        if (s.mouseIsPressed && e.target.id.startsWith('node')) {
+          let parts = e.target.id.split('-');
+          return {
+            ...s,
+            grid: getNewGridWithWallToggled(s.grid, parseInt(parts[1]), parseInt(parts[2])),
+            mouseIsPressed: true
+          };
+        } else {
+          return s;
         }
-        document.addEventListener('mousedown', handleMouseDown);
+      })
+    }
+    document.addEventListener('mouseenter', handleMouseEnter, true);
 
-        const handleMouseUp = () => {
-            setState(s => ({
-                ...s,
-                mouseIsPressed: false
-            }));
-        }
-        document.addEventListener('mouseup', handleMouseUp);
+    return () => {
+      document.removeEventListener('mousedown', handleMouseDown);
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('mouseenter', handleMouseEnter);
+    }
+  }, []);
 
-
-        const handleMouseEnter = (e) => {
-            setState(s => {
-                if (s.mouseIsPressed && e.target.id.startsWith('node')) {
-                    let parts = e.target.id.split('-');
-                    return {
-                        ...s,
-                        grid: getNewGridWithWallToggled(s.grid, parseInt(parts[1]), parseInt(parts[2])),
-                        mouseIsPressed: true
-                    };
-                } else {
-                    return s;
-                }
-            })
-        }
-        document.addEventListener('mouseenter', handleMouseEnter, true);
-
-        return () => {
-            document.removeEventListener('mousedown', handleMouseDown);
-            document.removeEventListener('mouseup', handleMouseUp);
-            document.removeEventListener('mouseenter', handleMouseEnter);
-        }
-    }, []);
-
-    return (
-        <>
-            <button>Visualize Dijkstra's Algorithm</button>
-            <button onClick={() => setState({
-                ...state,
-                grid: getInitialGrid()
-            })}>Clear Board</button>
-            <div className="grid">
-                {state.grid.map((row, rowIdx) => {
-                    return (
-                        <div key={rowIdx}>
-                            {row.map((node, colIdx) => {
-                                const { row, col, isFinish, isStart, isWall, ref } = node;
-                                return (
-                                    <Node
-                                        ref={ref}
-                                        row={row}
-                                        col={col}
-                                        key={colIdx}
-                                        isFinish={isFinish}
-                                        isStart={isStart}
-                                        isWall={isWall} />
-                                );
-                            })}
-                        </div>
-                    );
-                })}
+  return (
+    <>
+      <button>Visualize Dijkstra's Algorithm</button>
+      <button onClick={() => setState({
+        ...state,
+        grid: getInitialGrid()
+      })}>Clear Board</button>
+      <div className="grid">
+        {state.grid.map((row, rowIdx) => {
+          return (
+            <div key={rowIdx}>
+              {row.map((node, colIdx) => {
+                const { row, col, isFinish, isStart, isWall, ref } = node;
+                return (
+                  <Node
+                    ref={ref}
+                    row={row}
+                    col={col}
+                    key={colIdx}
+                    isFinish={isFinish}
+                    isStart={isStart}
+                    isWall={isWall} />
+                );
+              })}
             </div>
-        </>
-    );
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 function getInitialGrid() {
-    const grid = [];
-    for (let row = 0; row < 20; row++) {
-        const currentRow = [];
-        for (let col = 0; col < 50; col++) {
-            currentRow.push(createNode(row, col));
-        }
-        grid.push(currentRow);
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(row, col));
     }
-    return grid;
+    grid.push(currentRow);
+  }
+  return grid;
 }
 
 function createNode(row, col) {
-    const ref = React.createRef();
-    return {
-        row,
-        col,
-        ref,
-        isStart: row === START_NODE_ROW && col === START_NODE_COL,
-        isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
-        distance: Infinity,
-        seen: false,
-        isWall: false,
-        previousNode: null
-    }
+  const ref = React.createRef();
+  return {
+    row,
+    col,
+    ref,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    seen: false,
+    isWall: false,
+    previousNode: null
+  }
 }
 
 function getNewGridWithWallToggled(grid, row, col) {
-    const newGrid = grid.slice();
-    const node = newGrid[row][col];
-    const newNode = {
-        ...node,
-        isWall: !node.isWall,
-    };
-    newGrid[row][col] = newNode;
-    return newGrid;
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
 };
